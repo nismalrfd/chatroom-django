546a4d24d7bcb338eaa33c9030c39ef04e0e97e7
nismal <110675248+nismalrfd@users.noreply.github.com>
2023-01-10 19:24:41 +0530

    settings database

--- Tecturtles/settings.py
+++ Tecturtles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kx1jw+_ot4uj&um7qdsk)+$6#5cin#%#(dkuuy!!xp7y(oc*vj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -75,11 +75,21 @@ WSGI_APPLICATION = 'Tecturtles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'turtle_data',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'12345678',
+        'PORT':'3306',
+    }
 }
 
 
