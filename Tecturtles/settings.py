4395f22703430bcaff6cc6fa7ceea8eecdf05a12
nismal <110675248+nismalrfd@users.noreply.github.com>
2023-02-16 18:19:29 +0530

    Add files via upload

--- Tecturtles/settings.py
+++ Tecturtles/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kx1jw+_ot4uj&um7qdsk)+$6#5cin#%#(dkuuy!!xp7y(oc*vj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
