7c55eb8c69a0f2384fe9edb9b1e9fa121cd7f112
nismal <110675248+nismalrfd@users.noreply.github.com>
2023-04-02 11:04:05 +0530

    Update settings.py

--- techturtles/settings.py
+++ techturtles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2%qf9cxu1i_)+n9!t3ckc9fn8r5ykeaz_27_ei92=-ap#6m@t_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
