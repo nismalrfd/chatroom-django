1fdefb6ee93eef1a2eec84f6a72def329a1fd7dd
nismal <110675248+nismalrfd@users.noreply.github.com>
2023-07-15 10:06:33 +0530

    Delete base directory

--- base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from base.models import *
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
